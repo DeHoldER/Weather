e4cb5d86ba2d3d398c336af2a9c0a9f083ccbbc8
MrFox <stanislav-mk@mail.ru>
2021-09-02 23:58:27 +0300

    v 0.02 - реализована передача погоды через viewmodel - добавлен рандомизатор для appState

--- app/src/main/java/ru/geekbrains/weather/domain/City.kt
+++ app/src/main/java/ru/geekbrains/weather/domain/City.kt
@@ -4,6 +4,4 @@ data class City(
     val name: String = "Москва",
     val lon: Double = 55.755826,
     val lat: Double = 37.617299900000035,
-) {
-    fun getDefaultCity() = City("Москва", 55.755826, 37.617299900000035)
-}
+)
--- app/src/main/java/ru/geekbrains/weather/view/MainFragment.kt
+++ app/src/main/java/ru/geekbrains/weather/view/MainFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import ru.geekbrains.weather.R
 import ru.geekbrains.weather.databinding.FragmentMainBinding
+import ru.geekbrains.weather.domain.Weather
+import ru.geekbrains.weather.viewmodel.AppState
 import ru.geekbrains.weather.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
@@ -35,6 +40,59 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        val observer = Observer<AppState> { renderData(it) }
+        mainViewModel.getAppState().observe(viewLifecycleOwner, observer)
+        mainViewModel.getWeather()
+    }
+
+    private fun renderData(appState: AppState) {
+        when (appState) {
+            is AppState.Loading -> {
+                showLoadingBar()
+            }
+            is AppState.Success -> {
+                hideLoadingBar()
+                Snackbar.make(requireView(), "Success", Snackbar.LENGTH_LONG).show()
+                setData(appState.weatherData)
+
+            }
+            is AppState.Error -> {
+                Snackbar
+                    .make(requireView(), "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") { mainViewModel.getWeather() }
+                    .show()
+            }
+        }
+    }
+
+    private fun setData(weather: Weather) {
+        with(binding) {
+            cityName.text = weather.city.name
+            cityCoordinates.text = String.format(
+                getString(R.string.city_coordinates),
+                weather.city.lat.toString(),
+                weather.city.lon.toString(),
+            )
+            temperatureValue.text = weather.temperature.toString()
+            temperatureFeelsLike.text = weather.feelsLike.toString()
+        }
+    }
+
+
+    private fun showLoadingBar() {
+        with(binding) {
+            loadingBar.visibility = View.VISIBLE
+            infoBox.visibility = View.GONE
+        }
+    }
+
+    private fun hideLoadingBar() {
+        with(binding) {
+            loadingBar.visibility = View.GONE
+            infoBox.visibility = View.VISIBLE
+        }
     }
 
 }
