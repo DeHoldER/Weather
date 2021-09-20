6ed2028c4d9713e9a683602b3d2c52abb2fa71b3
MrFox <stanislav-mk@mail.ru>
2021-09-21 02:33:56 +0300

    v 0.09 - Подпиcался на событие изменения связи (CONNECTIVITY_ACTION) для уведомлений об этом пользователя.

--- app/src/main/java/ru/geekbrains/weather/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/weather/view/MainActivity.kt
@@ -1,7 +1,13 @@
 package ru.geekbrains.weather.view
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ru.geekbrains.weather.R
 import ru.geekbrains.weather.view.main.MainFragment
 
@@ -10,11 +16,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+        registerReceiver(networkStateReceiver, filter)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, MainFragment.newInstance())
                 .addToBackStack(null)
                 .commit()
         }
+    }
+
+    private var networkStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val noConnectivity =
+                intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false)
+            if (!noConnectivity) {
+                onConnectionFound()
+            } else {
+                onConnectionLost()
+            }
+        }
+    }
+
+    fun onConnectionLost() {
+        Toast.makeText(this, "Connection lost", Toast.LENGTH_LONG).show()
+    }
+
+    fun onConnectionFound() {
+        Toast.makeText(this, "Connection found", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(networkStateReceiver)
     }
 }
